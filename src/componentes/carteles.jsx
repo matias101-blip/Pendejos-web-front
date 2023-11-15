c1992e1276b0ff0027e516db01d0273ffc8ba8a7
TheHunter101 <thehunter10121@gmail.com>
2023-11-15 13:04:41 -0500

    Preparado

---
+++ src/componentes/carteles.jsx
@@ -0,0 +1,12 @@
+function Carteles() {
+  return(
+    <div className='Cartelera'>
+      <img src={require("../img/Tap-Water.png")} alt="portada" />
+      <div className='Info' >
+        <h2 className='titulo' >Tap Water Pollution</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Carteles;
